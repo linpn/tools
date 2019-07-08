d223e86c3b415c06176d1768973c2ddf9334dcfc
Linpn <lpn5200@qq.com>
2019-07-08 16:28:09 +0800

    通用工具包

--- src/com/github/linpn/webtools/filter/EncodingExtFilter.java
+++ src/com/github/linpn/webtools/filter/EncodingExtFilter.java
@@ -24,7 +24,6 @@ public class EncodingExtFilter extends CharacterEncodingFilter {
         HttpServletExtResponse res = new HttpServletExtResponse(response);
         req.setCharacterEncoding(this.encoding);
         res.setCharacterEncoding(this.encoding);
-        res.setContentType("text/html");
         super.doFilterInternal(req, res, filterChain);
     }
 
